d43513fd938f797e694889b7fd17f359717c2e84
Daniel <d685shvarts@gmail.com>
2019-06-22 11:28:02 -0700

    Updates

---
+++ precache-manifest.d3ce876aeb9a03ca8585377ea2dc9e4e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "650f56dbb1a5146035eea8d742995f54",
+    "url": "/dshvarts/static/media/website.650f56db.JPG"
+  },
+  {
+    "revision": "d0296362bacddc3bcf232eaf950f2431",
+    "url": "/dshvarts/static/media/home-background4.d0296362.jpg"
+  },
+  {
+    "revision": "a160501b1285314b6c248ef9ea4a32ce",
+    "url": "/dshvarts/static/media/about_me_photo.a160501b.jpg"
+  },
+  {
+    "revision": "b8573f3de11ccfb38c5b9d295a4cdb33",
+    "url": "/dshvarts/static/media/162_final.b8573f3d.JPG"
+  },
+  {
+    "revision": "d17913bdd5668d117f2e",
+    "url": "/dshvarts/static/js/runtime~main.8610753e.js"
+  },
+  {
+    "revision": "3efc1b3c7c645c70476b",
+    "url": "/dshvarts/static/js/main.f235a961.chunk.js"
+  },
+  {
+    "revision": "3a4bcbd0fd333507f34c",
+    "url": "/dshvarts/static/js/2.1a2d25ca.chunk.js"
+  },
+  {
+    "revision": "3efc1b3c7c645c70476b",
+    "url": "/dshvarts/static/css/main.8d33e986.chunk.css"
+  },
+  {
+    "revision": "3a4bcbd0fd333507f34c",
+    "url": "/dshvarts/static/css/2.1d24d165.chunk.css"
+  },
+  {
+    "revision": "118baf8ebfa6438c0f1cf0f56310f77b",
+    "url": "/dshvarts/index.html"
+  }
+];
